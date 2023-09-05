61a31f1f7d4c5a19eb3eda47cdfbaf8cbf846339
DSRN0000 <dsrn0000@gmail.com>
2023-09-05 21:03:26 +0900

    second upload

--- gridy_algorithm/Q1_law_of_large_numbers_2.py
+++ gridy_algorithm/Q1_law_of_large_numbers_2.py
@@ -1,24 +1,3 @@
-# 단순하게 푸는 답안
-n,m,k = map(int,input().split())
-data = list(map(int,input().split()))
-data.sort()
-first = data[n-1]
-second = data[n-2]
-
-result = 0
-
-while True:
-    for i in range(k):
-        if m == 0:
-            break
-        result += first
-        m -= 1
-    if m == 0:
-        break
-    result += second
-    m -= 1
-print(result)
-
 # 시간복잡도를 고려한 답안
 n,m,k = map(int,input().split()) #n,m,k를 공백으로 구분하여 입력받기
 data = list(map(int,input().split())) #n개의 수를 공백으로 구분하여 입력받기
