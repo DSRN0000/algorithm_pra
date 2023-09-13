646f87395f18c6aa8315604581e13fc12fab002b
DSRN0000 <dsrn0000@gmail.com>
2023-09-13 21:29:55 +0900

    테스트케이스를 고려하지 않고 정말 단순하게 푼 것

---
+++ gridy_algorithm/go_to_one.py
@@ -0,0 +1,12 @@
+a,b = map(int,input().split())
+count = 0
+while a >= 1:
+    if a == 1:
+        break
+    if a%b == 0:
+        a /= b
+        count += 1
+    else:
+        a -= 1
+        count += 1
+print(count)
